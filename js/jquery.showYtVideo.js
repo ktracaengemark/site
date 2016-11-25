6182af016a18f33535194790591398e8d491a38b
Rodrigo Campos <rpcrodrigo@gmail.com>
2016-11-25 20:10:38 -0200

    INÍCIO DO REPOSITÓRIO: Primeira carga de arquivos do site no repositório site.

---
+++ js/jquery.showYtVideo.js
@@ -0,0 +1,1 @@
+<a href="#" onclick="window.open('http://codigofonte.net', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO,SCROLLBARS=NO, TOP=10, LEFT=10, WIDTH=800, HEIGHT=600');">Clique para abrir a janela POP-up</a>
